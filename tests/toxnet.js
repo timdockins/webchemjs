0e152d240e97ec5852a11ea19c9f7e3f0e824aeb
tim <timdockins@gmail.com>
2016-10-23 20:44:06 -0500

    cleaned up toxnet.js and made sure it was returning the right promise results. toxnet test seems to work now added lodash for processing stuff

--- tests/toxnet.js
+++ tests/toxnet.js
@@ -2,58 +2,114 @@
 
 const ToxNet = require("../index.js").ToxNet;
 
-const expect = require('chai').expect;
+const Q = require("q");
+const _ = require("lodash");
+const https = require("https");
 
 const util = require("util");
 
+
 ToxNet.initialQuery(
     {
-        "db" : "toxline",
-        keywords : "agranulocytosis",
-        verbose : false
-    },
-    function( err, results ) {
-        console.log("Final Results", util.inspect(results, false, null));        
-        var temporaryfile = results[0].value.queryresult.temporaryfile;
-
-        ToxNet.secondQuery(
-            {
-                "temporaryfile" : temporaryfile,
-                "start"         : 0,
-                "verbose"       : false
-            },
-            function(err, results) {
-                // console.log("secondQuery: ", util.inspect(results, false, null));
-
-                var docnumbers = results.docsumset.id.split(' ');
-
-                console.log("Fetching full records", docnumbers.slice(0,-1));
-                
-                docnumbers.forEach( function(docno) {
-                    console.log('Fetching RNs : ', docno);
-                    ToxNet.getFullRecord(
-                        {
-                            'db' : 'toxline',
-                            'docno' : docno,
-                            'verbose' : false
-                        },
-                        function( err, results) {
-                            // console.log("Full Document:", util.inspect(results, false, null));
-
-                            var rn = results.doclist.doc.hasOwnProperty('rn') ? results.doclist.doc.rn : [];
-
-                            rn.forEach(function(r) {
-                                console.log("RN:", r);    
-                            })
-
-                        }
-                    );                    
-                });
-                                
-                
-
-            }
-        );
-
+        db: "toxline",
+        keywords: "agranulocytosis",
+        verbose: false
     }
 )
+    .then(
+        function (results) {
+            console.log("Initial results", util.inspect(results, false, null));
+            var temporaryfile = results.queryresult.temporaryfile;
+            var total = parseInt(results.queryresult.count);
+            var displayMax = parseInt(results.queryresult.dispmax);
+            console.log("There are " + total + " documents.");
+
+            return ToxNet.secondQuery(
+                {
+                    temporaryfile: temporaryfile,
+                    start: 0,
+                    verbose: false
+                }
+            )
+
+        }
+    ).then(
+    function (results) {
+        console.log("Second results", util.inspect(results, false, null));
+        var docnumbers = results.docsumset.id.split(' ');
+
+        console.log("Fetching full records", docnumbers.slice(0, -1));
+        var docNumberPromises = [];
+
+        docnumbers.forEach(function (docno) {
+            docNumberPromises.push(ToxNet.getFullRecord(
+                {
+                    'db': 'toxline',
+                    'docno': docno,
+                    'verbose': false
+                }
+            ));
+
+        });
+
+        return Q.allSettled(docNumberPromises);
+    }
+).then(function (results) {
+    console.log("Full Document results ", util.inspect(results, false, null));
+    results = results.slice(0,-1);
+    results = results.map(function(result){
+        return result.value.doclist.doc;
+    });
+    // filter out documents that don't have a compound listed
+
+    var filteredResults = results.filter( function( doc ) {
+        return doc.hasOwnProperty('rn');
+    });
+
+
+    var compoundsFound = [];
+
+
+    filteredResults.forEach( function( doc ) {
+        if (Array.isArray(doc.rn)){
+            doc.rn.forEach( function( compound ) {
+                compoundsFound.push( constructHomeBaseCompoundRecord( "unii", compound, "TOXLINE", doc.docno ) );
+            })
+        } else {
+            compoundsFound.push( constructHomeBaseCompoundRecord( "unii", doc.rn, "TOXLINE", doc.docno ) );
+        }
+    });
+
+
+    console.log("Found " + compoundsFound.length + " compounds listed in documents.  Some may be duplicate. ");
+    return compoundsFound;
+
+})
+    .then(
+        function( compounds ) {
+            console.log("Compounds found: ", util.inspect(compounds, false, null ));
+        }
+    );
+
+
+
+
+
+
+function constructHomeBaseCompoundRecord( identifierType, identifier, source, docno ) {
+    var newCompound = {
+        recordType : "compound",
+        identifiers : {
+
+        },
+        documentation : [
+            {
+                source : source,
+                recordIdentifier : docno
+            }
+        ]
+    };
+
+    newCompound.identifiers[ identifierType.toLowerCase() ] = identifier.trim();
+    return newCompound;
+}
