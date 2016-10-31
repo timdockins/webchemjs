c47428962bd851612d7fea99a30930641e6ff212
tim <timdockins@gmail.com>
2016-10-30 21:36:15 -0500

    Added some RxNav calls

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports.CIR = require('./lib/cir');
 module.exports.ChemID = require('./lib/chemid');
 module.exports.PubChem = require('./lib/pubchem');
 module.exports.ToxNet = require("./lib/toxnet");
+module.exports.NDFRT = require("./lib/rxnav").NDFRT;
