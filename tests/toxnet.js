cbe98b2873b2f7cf8d84b5f52c9d6ff2597926c0
tim <timdockins@gmail.com>
2016-11-06 13:30:01 -0600

    Added POST type request to toxnet

--- tests/toxnet.js
+++ tests/toxnet.js
@@ -1,111 +1,43 @@
 "use const";
 
-const ToxNet = require("../index.js").ToxNet;
+const ToxNet = require( "../index.js" ).ToxNet;
 
-const Q = require("q");
-const _ = require("lodash");
-const https = require("https");
+const Q = require( "q" );
+const _ = require( "lodash" );
+const https = require( "https" );
 
-const util = require("util");
+const parseString = require( "xml2js" ).parseString;
 
+const util = require( "util" );
 
-ToxNet.initialQuery(
-    {
-        db: "toxline",
-        keywords: "agranulocytosis",
-        verbose: false
-    }
-)
+ToxNet.initialPost( {
+        db      : "toxline",
+        keywords: "agranulocytosis clozapine",
+        all     : true,
+        verbose : false
+    } )
     .then(
-        function (results) {
-            console.log("Initial results", util.inspect(results, false, null));
+        function ( results ) {
+            console.log( "Initial Results: ", util.inspect( results, false, null ) );
             var temporaryfile = results.queryresult.temporaryfile;
-            var total = parseInt(results.queryresult.count);
-            var displayMax = parseInt(results.queryresult.dispmax);
-            console.log("There are " + total + " documents.");
-
-            return ToxNet.secondQuery(
-                {
-                    temporaryfile: temporaryfile,
-                    start: 0,
-                    verbose: false
-                }
-            )
-
-        }
-    ).then(
-    function (results) {
-        console.log("Second results", util.inspect(results, false, null));
-        var docnumbers = results.docsumset.id.split(' ');
-
-        console.log("Fetching full records", docnumbers.slice(0, -1));
-        var docNumberPromises = [];
-
-        docnumbers.forEach(function (docno) {
-            docNumberPromises.push(ToxNet.getFullRecord(
-                {
-                    'db': 'toxline',
-                    'docno': docno,
-                    'verbose': false
-                }
-            ));
-
-        });
-
-        return Q.allSettled(docNumberPromises);
-    }
-).then(function (results) {
-    console.log("Full Document results ", util.inspect(results, false, null));
-    results = results.slice(0,-1);
-    results = results.map(function(result){
-        return result.value.doclist.doc;
-    });
-    // filter out documents that don't have a compound listed
-
-    var filteredResults = results.filter( function( doc ) {
-        return doc.hasOwnProperty('rn');
-    });
-
-
-    var compoundsFound = [];
-
-
-    filteredResults.forEach( function( doc ) {
-        if (Array.isArray(doc.rn)){
-            doc.rn.forEach( function( compound ) {
-                compoundsFound.push( constructHomeBaseCompoundRecord( "unii", compound, "TOXLINE", doc.docno ) );
-            })
-        } else {
-            compoundsFound.push( constructHomeBaseCompoundRecord( "unii", doc.rn, "TOXLINE", doc.docno ) );
-        }
-    });
-
-
-    console.log("Found " + compoundsFound.length + " compounds listed in documents.  Some may be duplicate. ");
-    return compoundsFound;
-
-})
-    .then(
-        function( compounds ) {
-            console.log("Compounds found: ", util.inspect(compounds, false, null ));
+            var total = parseInt( results.queryresult.count );
+            var displayMax = parseInt( results.queryresult.dispmax );
+            console.log( "There are " + total + " documents." );
+        },
+        function ( err ) {
+            console.err( "Error: ", err );
         }
     );
 
 
-
-
-
-
 function constructHomeBaseCompoundRecord( identifierType, identifier, source, docno ) {
     var newCompound = {
-        recordType : "compound",
-        identifiers : {
-
-        },
-        documentation : [
+        recordType   : "compound",
+        identifiers  : {},
+        documentation: [
             {
-                source : source,
-                recordIdentifier : docno
+                source          : source,
+                recordIdentifier: docno
             }
         ]
     };
